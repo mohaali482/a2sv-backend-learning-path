58d48c36dacdd502ae3bbdbfb8567822b58332aa
Mohammed Ali <mohaali482@gmail.com>
2024-07-30 17:43:39 +0300

    feat: finished working on task2, second task

--- task-2/cmd/palindrome-check/main.go
+++ task-2/cmd/palindrome-check/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/mohaali/a2sv-backend-learning-path/task-2/pkg"
+)
+
 func main() {
+	fmt.Println("                              Palindrome Check ✅/🚫")
+	fmt.Println("⚠⚠This checker is case-insensitive, ignores spaces and ignores punctuation marks.⚠⚠")
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("Enter your word: ")
+	str := pkg.StringInput()
+	fmt.Println()
+
+	palindrome := pkg.PalindromeCheck(str)
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("Your word is ")
+	if palindrome {
+		fmt.Printf("a palindrome ✅.")
+	} else {
+		fmt.Printf("not a palindrome 🚫.")
+
+	}
+	fmt.Println()
+	fmt.Println()
 
 }
