79096968737aca0095bc01e4ae1b234d9a8b1ab9
Mohammed Ali <mohaali482@gmail.com>
2024-07-30 17:39:23 +0300

    feat: finished working on task2, first task

--- task-2/cmd/word-frequency-count/main.go
+++ task-2/cmd/word-frequency-count/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/mohaali/a2sv-backend-learning-path/task-2/pkg"
+)
+
 func main() {
+	fmt.Println("                              Word Counter ⏱")
+	fmt.Println("⚠⚠This counter is case-insensitive and ignores punctuation marks.⚠⚠")
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("Enter your sentence: ")
+	str := pkg.StringInput()
+	fmt.Println()
+	counter := pkg.CountWords(str)
+
+	fmt.Println()
+	fmt.Println("Word     Count")
+	fmt.Println()
+	for word, count := range counter {
+		fmt.Printf("%-5v .......... %v\n", word, count)
+	}
+	fmt.Println()
+	fmt.Println()
 
 }
