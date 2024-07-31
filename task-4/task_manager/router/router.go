52e9ec0157f9aa055ba5992246509fae6fccb48f
Mohammed Ali <mohaali482@gmail.com>
2024-07-31 17:54:01 +0300

    feat: add gin router to the controllers

--- task-4/task_manager/router/router.go
+++ task-4/task_manager/router/router.go
@@ -2,9 +2,14 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mohaali482/a2sv-backend-learning-path/task-4/controllers"
 	"github.com/mohaali482/a2sv-backend-learning-path/task-4/data"
 )
 
 func RegisterHandlers(r *gin.Engine, s data.TaskUseCase) {
-
+	r.GET("/tasks", controllers.GetAllTasks(s))
+	r.GET("/tasks/:id", controllers.GetTaskById(s))
+	r.PUT("/tasks/:id", controllers.UpdateTask(s))
+	r.DELETE("/tasks/:id", controllers.DeleteTask(s))
+	r.POST("/tasks", controllers.CreateTask(s))
 }
