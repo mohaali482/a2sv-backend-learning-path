f97a6a45d5f256457b7feda7535ec949a8943bf1
Mohammed Ali <mohaali482@gmail.com>
2024-07-31 17:39:02 +0300

    feat: add a controller to get task by id

--- task-4/task_manager/controllers/task_controller.go
+++ task-4/task_manager/controllers/task_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mohaali482/a2sv-backend-learning-path/task-4/data"
@@ -10,5 +11,29 @@ import (
 func GetAllTasksController(s data.TaskUseCase) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, s.GetAllTasks())
+	}
+}
+
+func GetTaskById(s data.TaskUseCase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		intId, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid id",
+			})
+			return
+		}
+
+		task, err := s.GetTaskById(intId)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "task not found",
+			})
+		}
+
+		c.JSON(http.StatusOK, task)
 	}
 }
