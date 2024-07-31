a3316670c1cfc2a474abc068ec8d10cf0befff29
Mohammed Ali <mohaali482@gmail.com>
2024-07-31 10:59:24 +0300

    feat: add receiver functions to manipulate borrowed books

--- task-3/library_management/models/member.go
+++ task-3/library_management/models/member.go
@@ -1,7 +1,29 @@
 package models
 
+import "errors"
+
 type Member struct {
 	ID            int
 	Name          string
 	BorrowedBooks []Book
+}
+
+func (m *Member) FindBook(bookId int) (int, error) {
+	for idx, book := range m.BorrowedBooks {
+		if book.ID == bookId {
+			return idx, nil
+		}
+	}
+
+	return 0, errors.New("book not borrowed by member")
+}
+
+func (m *Member) RemoveBook(idx int) (bool, error) {
+	if idx < len(m.BorrowedBooks) {
+		m.BorrowedBooks[idx], m.BorrowedBooks[len(m.BorrowedBooks)-1] = m.BorrowedBooks[len(m.BorrowedBooks)-1], m.BorrowedBooks[idx]
+		m.BorrowedBooks = m.BorrowedBooks[:idx]
+		return true, nil
+	}
+
+	return false, errors.New("index out of bound")
 }
