9d337b5a7d618f57013da4f716b44c3bd9a65035
Mohammed Ali <mohaali482@gmail.com>
2024-07-31 17:22:37 +0300

    feat: add implementation of task use-cases using task service

--- task-4/task_manager/data/task_service.go
+++ task-4/task_manager/data/task_service.go
@@ -1,6 +1,9 @@
 package data
 
 import (
+	"errors"
+	"sync"
+
 	"github.com/mohaali482/a2sv-backend-learning-path/task-4/models"
 )
 
@@ -9,5 +12,91 @@ type TaskUseCase interface {
 	GetTaskById(id int) (*models.Task, error)
 	UpdateTask(id int, task models.Task) (*models.Task, error)
 	DeleteTask(id int) error
-	CreateTask(task models.Task) error
+	CreateTask(task models.Task)
+}
+
+type TaskService struct {
+	nextId int
+	tasks  map[int]*models.Task
+	mu     sync.RWMutex
+}
+
+func NewTaskService() *TaskService {
+	return &TaskService{
+		nextId: 1,
+		tasks:  make(map[int]*models.Task),
+		mu:     sync.RWMutex{},
+	}
+}
+
+func (s *TaskService) GetAllTasks() []*models.Task {
+	s.mu.RLock()
+
+	tasks := make([]*models.Task, 0)
+	for _, task := range s.tasks {
+		tasks = append(tasks, task)
+	}
+	s.mu.RUnlock()
+
+	return tasks
+}
+
+func (s *TaskService) GetTaskById(id int) (*models.Task, error) {
+	s.mu.RLock()
+	task, ok := s.tasks[id]
+	s.mu.RUnlock()
+
+	if !ok {
+		return &models.Task{}, errors.New("task not found")
+	}
+
+	return task, nil
+}
+
+func (s *TaskService) UpdateTask(id int, task models.Task) (*models.Task, error) {
+	oldTask, err := s.GetTaskById(id)
+
+	if err != nil {
+		return &models.Task{}, err
+	}
+
+	s.mu.Lock()
+
+	oldTask.Title = task.Title
+	oldTask.Done = task.Done
+
+	s.mu.Unlock()
+
+	return oldTask, nil
+}
+
+func (s *TaskService) DeleteTask(id int) error {
+	_, err := s.GetTaskById(id)
+	if err != nil {
+		return err
+	}
+
+	s.mu.Lock()
+
+	delete(s.tasks, id)
+
+	s.mu.Unlock()
+
+	return nil
+}
+
+func (s *TaskService) CreateTask(task models.Task) {
+	newTask := &models.Task{
+		Title: task.Title,
+		Done:  false,
+	}
+
+	s.mu.Lock()
+
+	newTask.Id = s.nextId
+	s.nextId++
+	s.tasks[newTask.Id] = newTask
+
+	s.mu.Unlock()
+
 }
