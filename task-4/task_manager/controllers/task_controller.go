3167185e4d77db9813ccb8520a4f5050bd4ee133
Mohammed Ali <mohaali482@gmail.com>
2024-07-31 17:52:36 +0300

    feat: change GetAllTasksController to GetAllTasks

--- task-4/task_manager/controllers/task_controller.go
+++ task-4/task_manager/controllers/task_controller.go
@@ -9,7 +9,7 @@ import (
 	"github.com/mohaali482/a2sv-backend-learning-path/task-4/models"
 )
 
-func GetAllTasksController(s data.TaskUseCase) gin.HandlerFunc {
+func GetAllTasks(s data.TaskUseCase) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, s.GetAllTasks())
 	}
---
+++ task-4/task_manager/router/router.go
@@ -0,0 +1,10 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mohaali482/a2sv-backend-learning-path/task-4/data"
+)
+
+func RegisterHandlers(r *gin.Engine, s data.TaskUseCase) {
+
+}
