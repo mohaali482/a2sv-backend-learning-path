7321e4a67daed2871bd71978a58b2da1682fbf1d
Mohammed Ali <mohaali482@gmail.com>
2024-08-03 15:18:32 +0300

    feat: change the type of id

--- task-6/task_manager/models/task.go
+++ task-6/task_manager/models/task.go
@@ -1,13 +1,16 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Task struct {
-	Id     string `json:"id" bson:"_id,omitempty"`
-	UserId string `json:"user_id"`
-
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	DateTime    time.Time `json:"datetime"`
-	Done        bool      `json:"done"`
+	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	UserId      primitive.ObjectID `json:"user_id"`
+	Title       string             `json:"title"`
+	Description string             `json:"description"`
+	DateTime    time.Time          `json:"datetime"`
+	Done        bool               `json:"done"`
 }
