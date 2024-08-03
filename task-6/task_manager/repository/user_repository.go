f73a07d7853ff254031f61bd78fdead297a7af60
Mohammed Ali <mohaali482@gmail.com>
2024-08-03 15:35:46 +0300

    feat: add context for the repositories

--- task-6/task_manager/repository/user_repository.go
+++ task-6/task_manager/repository/user_repository.go
@@ -1,10 +1,14 @@
 package repository
 
-import "github.com/mohaali482/a2sv-backend-learning-path/task-6/models"
+import (
+	"context"
+
+	"github.com/mohaali482/a2sv-backend-learning-path/task-6/models"
+)
 
 type UserRepository interface {
-	CreateUser(user *models.User) error
-	GetUserByID(id int) (*models.User, error)
-	UpdateUser(user *models.User) error
-	DeleteUser(id int) error
+	CreateUser(ctx context.Context, user *models.User) error
+	GetUserByID(ctx context.Context, id int) (*models.User, error)
+	UpdateUser(ctx context.Context, user *models.User) error
+	DeleteUser(ctx context.Context, id int) error
 }
