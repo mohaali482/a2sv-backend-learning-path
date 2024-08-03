2eed4d567de2771f96e96e3d0d5f6f2b966f0c4d
Mohammed Ali <mohaali482@gmail.com>
2024-08-03 15:37:22 +0300

    feat: add return type for create user

--- task-6/task_manager/repository/user_repository.go
+++ task-6/task_manager/repository/user_repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 type UserRepository interface {
-	CreateUser(ctx context.Context, user *models.User) error
+	CreateUser(ctx context.Context, user *models.User) (*models.User, error)
 	GetUserByID(ctx context.Context, id int) (*models.User, error)
 	UpdateUser(ctx context.Context, user *models.User) error
 	DeleteUser(ctx context.Context, id int) error
