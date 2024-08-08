53db133fa3da9937c68ef488512e8d5572e4d77c
Mohammed Ali <mohaali482@gmail.com>
2024-08-08 15:31:57 +0300

    feat: mapped the user controllers with gin router

--- task-6/task_manager/router/router.go
+++ task-6/task_manager/router/router.go
@@ -4,12 +4,27 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mohaali482/a2sv-backend-learning-path/task-6/controllers"
 	"github.com/mohaali482/a2sv-backend-learning-path/task-6/data"
+	"github.com/mohaali482/a2sv-backend-learning-path/task-6/middleware"
 )
 
-func RegisterHandlers(r *gin.Engine, s data.TaskUseCase) {
-	r.GET("/tasks", controllers.GetAllTasks(s))
-	r.GET("/tasks/:id", controllers.GetTaskById(s))
-	r.PUT("/tasks/:id", controllers.UpdateTask(s))
-	r.DELETE("/tasks/:id", controllers.DeleteTask(s))
-	r.POST("/tasks", controllers.CreateTask(s))
+func RegisterTaskHandlers(r *gin.Engine, ts data.TaskUseCase, us data.UserUseCase) {
+	taskRouter := r.Group("/tasks", middleware.AuthMiddleware(us))
+
+	{
+		taskRouter.GET("", controllers.GetAllTasks(ts))
+		taskRouter.GET("/:id", controllers.GetTaskById(ts))
+
+		taskRouter.Use(middleware.AdminMiddleware(us))
+		taskRouter.PUT("/:id", controllers.UpdateTask(ts))
+		taskRouter.DELETE("/:id", controllers.DeleteTask(ts))
+		taskRouter.POST("", controllers.CreateTask(ts))
+	}
+}
+
+func RegisterUserHandlers(r *gin.Engine, us data.UserUseCase) {
+	r.POST("/login", controllers.Login(us))
+	r.POST("/register", controllers.Register(us))
+
+	r.Use(middleware.AdminMiddleware(us))
+	r.POST("/promote", controllers.Promote(us))
 }
